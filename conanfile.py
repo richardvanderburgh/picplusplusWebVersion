c16217865c46b10b213c7fd433d814be2ee0768e
Jose Salinas <j.jr.2011@outlook.com>
2024-01-18 20:35:59 -0500

    Added few things

--- conanfile.py
+++ conanfile.py
@@ -12,6 +12,7 @@ class pic_plus_plus(ConanFile):
     def requirements(self):
         self.requires("gtest/1.13.0")
         self.requires("nlohmann_json/3.11.2")
+        self.requires("sdl/2.28.5")
 
     def build(self):
         cmake = CMake(self)
